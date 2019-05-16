f23eaf969d172aaf33ae5e15d28705444652814d
杨翰文 <yanghanwen@36kr.com>
2019-05-16 15:01:19 +0800

    feat: add visual editor

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+import 'visual-editor/lib/visualEditor.css';
+export default {
+  name: 'soda',
+};
+</script>
+
+<style>
+/* CSS */
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="soda-home">
+    <draw />
+  </div>
+</template>
+
+<script>
+  import Draw from 'visual-editor';
+
+  export default {
+    name: 'landing-page',
+    components: { Draw },
+    methods: {
+      open(link) {
+        this.$electron.shell.openExternal(link);
+      },
+    },
+  };
+</script>
+
+<style lang="less">
+  @import '../assets/styles/vars.less';
+  @import '../assets/styles/mixins.less';
+  .soda-home {
+    position: absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    background: @gray-light;
+  }
+</style>
