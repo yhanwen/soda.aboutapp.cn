8c162c37c977eabbb167bce2e07ce24c73519a13
杨翰文 <yanghanwen@36kr.com>
2019-05-31 09:22:06 +0800

    feat: autorun only run in development mode

--- src/renderer/pages/LandingPage.vue
+++ src/renderer/pages/LandingPage.vue
@@ -23,8 +23,11 @@ export default {
     return {};
   },
   async mounted() {
-    AutoRun.openLabel(this);
-    // AutoRun.openEditor(this);
+    if (process.env.NODE_ENV === 'development') {
+      // 开发环境启动自动执行的脚本
+      AutoRun.openLabel(this);
+      // AutoRun.openEditor(this);
+    }
   },
   computed: {},
   methods: {},
